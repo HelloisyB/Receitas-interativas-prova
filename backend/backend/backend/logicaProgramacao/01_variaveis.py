9e81886b39e026908a4b52688f6206b943c3bd8f
Helloisy <helloisybeatriz9@gmail.com>
2025-05-21 11:46:51 -0300

    ajustes finais e logica de programação

---
+++ backend/backend/backend/logicaProgramacao/01_variaveis.py
@@ -0,0 +1,31 @@
+nome= "Helloisy"
+idade= "17"
+salario= 1.512
+legal= True
+
+print("nomd", nome)
+print("idade", idade)
+print("salario", salario)
+print("legal", legal)
+
+animal= "cobra"
+peso= 170
+raca= "Píton-reticulada"
+agressivo= True
+
+print("animal", animal)
+print("peso", peso)
+print("raca", raca)
+print("agrssivo", agressivo)
+
+tipo = "Camiseta"
+tamanho = "M"
+cor = "Preta"
+preco = 59.90
+disponivel = True
+
+print("Tipo", tipo)
+print("Tamanho", tamanho)
+print("Cor", cor)
+print("Preço", preco)
+print("Disponível", disponivel)
---
+++ backend/backend/backend/logicaProgramacao/02_operacoesMatematicas.py
@@ -0,0 +1,23 @@
+a= 10
+b=  5
+soma= a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+
+print("Resultado da soma é:", soma)
+print("Resultado da Subtração é:", subtracao)
+print("Resultado da multiplicação é:", multiplicacao)
+print("Resultado da divisão é:", divisao)
+
+n= 8
+o= 5
+soma= n + o
+subtracao = n - o
+multiplicacao = n * o
+divisao = n / o
+
+print("Resultado da soma é:", soma)
+print("Resultado da Subtração é:", subtracao)
+print("Resultado da multiplicação é:", multiplicacao)
+print("Resultado da divisão é:", divisao)
