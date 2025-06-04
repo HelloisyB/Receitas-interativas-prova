8daff16525242edcb1a634aa819bad76e593ab2d
helloisy <helloisybeatriz9@gmail.com>
2025-06-04 11:50:18 -0300

    finalizado

---
+++ backend/backend/backend/logicaProgramacao/05_testando.py
@@ -0,0 +1,8 @@
+salario = int(input("Sua classe de salario é:"))
+
+if(salario >= 5.000):
+    print("Classe alta")
+elif (salario >= 3000):
+    print("Classe média")
+else:
+    print("Classe baixa")
