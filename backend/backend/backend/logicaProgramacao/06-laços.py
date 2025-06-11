43619eda4ec2300e39b7807b64da26e8db558272
helloisy <helloisybeatriz9@gmail>
2025-06-11 11:53:19 -0300

    fazendo atv

---
+++ backend/backend/backend/logicaProgramacao/06-laços.py
@@ -0,0 +1,15 @@
+#for (para)
+for i in range(5):
+  print("contando:", i)
+
+#while (enquando)
+senha = ""
+while senha != "hello":
+ senha = input("digite a senha: ")
+ print("Senha incorreta")
+
+print("Acesso liberado.")
+
+#atividade mostar numeros de 1 a 10 com for
+for i in range(1, 11):  # range(1, 11) vai de 1 até 10
+    print("Mostrar:", i)
---
+++ backend/backend/backend/logicaProgramacao/07-def.py
@@ -0,0 +1,19 @@
+#crie uma função que recebe um nome e diga "olá [nome]"
+def saudacao(nome):
+    print(f"Olá {nome}")
+
+print( saudacao ("Hello"))
+
+#receber dois numeros e retorne a media
+def media(t1, t2, t3):
+    return (t1 + t2 + t3) / 3
+
+print (media (98, 80, 100)) #media(98, 80)
+
+#dizer se um numero é par
+def par(numero):
+    if (numero % 2 == 0):
+     print("é par: ")
+     
+    else:
+print
