bb27b3a9148637b3c4ba6ab55ee7231c16089e64
helloisy <helloisybeatriz9@gmail.com>
2025-09-05 11:49:51 -0300

    veiculo

--- backend/backend/backend/logicaProgramacao/livros.py
+++ backend/backend/backend/logicaProgramacao/livros.py
@@ -5,49 +5,80 @@
 import json 
 
 livros = []
-try:
-    with open('biblioteca.json', 'r') as arquivo:
-        livros = json.load(arquivo)
-        print("Arquivo carregado!")
-except FileNotFoundError:
-    print("Arquivo nao encontrado")
 
-print("\n---- Cadastrar livro ----")
-titulo = input("Digite o título do livro: ")
-
-autor = input("Digite o nome do autor: ")
-
-editora = input("Digite a editora: ")
-
-while True:
+def carregarArquivo():
     try:
-        ano_publicacao = int(input("Digite o ano de publicação: "))
-        break
-    except ValueError:
-        print("Digite apenas numeros para o ano.")
+        with open('biblioteca.json', 'r') as arquivo:
+            livros = json.load(arquivo)
+            print("Arquivo carregado!")
+    except FileNotFoundError:
+        print("Arquivo nao encontrado")
 
-while True:
-    try:  
-        numero_paginas = int(input("Digite o número de páginas: ")) 
-        break
-    except ValueError:
-        print("Digite apenas numeros para páginas.")
+def cadastrarLivro():
 
-genero = input("Digite o gênero: ")
-idioma = input("Digite o idioma: ")
+    print("\n---- Cadastrar livro ----")
+    titulo = input("Digite o título do livro: ")
 
-novo_livro = {
-    "titulo": titulo,
-    "autor": autor,
-    "editora": editora,
-    "ano_publicacao": ano_publicacao,
-    "genero": genero,
-    "numero_paginas": numero_paginas,
-    "idioma": idioma
-}
+    autor = input("Digite o nome do autor: ")
 
-livros.append(novo_livro)
-with open('biblioteca.json', 'w') as arquivo:
-    json.dump(livros, arquivo, indent=4)
+    editora = input("Digite a editora: ")
 
-print(f"\nLivro '{titulo}' foi cadastrado com sucesso.")
+    while True:
+        try:
+            ano_publicacao = int(input("Digite o ano de publicação: "))
+            break
+        except ValueError:
+            print("Digite apenas numeros para o ano.")
+
+    while True:
+        try:  
+            numero_paginas = int(input("Digite o número de páginas: ")) 
+            break
+        except ValueError:
+            print("Digite apenas numeros para páginas.")
+
+    genero = input("Digite o gênero: ")
+    idioma = input("Digite o idioma: ")
+
+    novo_livro = {
+        "titulo": titulo,
+        "autor": autor,
+        "editora": editora,
+        "ano_publicacao": ano_publicacao,
+        "genero": genero,
+        "numero_paginas": numero_paginas,
+        "idioma": idioma
+    }
+
+    livros.append(novo_livro)
+    with open('biblioteca.json', 'w') as arquivo:
+        json.dump(livros, arquivo, indent=4)
+
+    print(f"\nLivro '{titulo}' foi cadastrado com sucesso.")
+
+
+# i++ --> i=i+1 (auto incremento)
+
+# inventario = []
+# with open ("biblioteca.json", 'r') as biblioteca:
+# produto_para_alterar = int(input("digite o id do produto"))
+# inventario = json.load(biblioteca)
+# for livro in inventario
+# if produto_para_alterar == livro["id"]:
+# nome_nome = input("digite o novo nome")
+# livro["nome"]= novo_nome
+#with open("bivlioteca.json", 'w') as biblioteca:
+#json.dump(inventario, biblioteca, indent=4)
+
+
+
+#inventario = []
+#with open("biblioteca.json", "r") as biblioteca:
+    #livro_para_excluir = int(input("Informe o ID do livro: "))
+    #inventario = json.load(biblioteca)
+#novo_inventario = []
+#for livro in inventario:
+ #   if livro_para_excluir != livro["id"]:
+      #  novo_inventario.append(livro)
+#with open("biblioteca.json", "w") as biblioteca:
+    #json.dump(novo_inventario, biblioteca, indent=4)
